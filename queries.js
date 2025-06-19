5d907ad08b7aca7d9de4f4b1348e4503ecd1128a
sudhan-1999 <jsudhan68@gmail.com>
2025-06-19 10:55:00 +0530

    dashboard,expeded endpoint created and register ednpoint modified

--- queries.js
+++ queries.js
@@ -1,49 +1,146 @@
-import { MongoClient } from 'mongodb';
-import dotenv from 'dotenv';
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
 dotenv.config();
 
-const mongo=process.env.MONGO_URL;
+const mongo = process.env.MONGO_URL;
 
 async function connectToMongo() {
-     const client=new MongoClient(mongo);
- await client.connect();
- console.log("Connected to MongoDB");
- return client;
+  const client = new MongoClient(mongo);
+  await client.connect();
+  console.log("Connected to MongoDB");
+  return client;
 }
 
-export const client=await connectToMongo();
+export const client = await connectToMongo();
 
 //asset_management.Purchase users
 export const checkuser = async (userid) => {
-   let user=await client.db("asset_management").collection("users").findOne({ userid: userid });
-   return user
+  let user = await client
+    .db("asset_management")
+    .collection("users")
+    .findOne({ userid: userid });
+  return user;
 };
 
 export const registeruser = async (userData) => {
-  return await client.db("asset_management").collection("users").insertOne(userData);
+  return await client
+    .db("asset_management")
+    .collection("users")
+    .insertOne(userData);
+};
+
+export async function getAllPurchases(filter = {}) {
+  return client
+    .db("asset_management")
+    .collection("Purchase")
+    .find(filter)
+    .toArray();
 }
 
-export async function getAllPurchases(filter={}) {
-  return client.db("asset_management").collection("Purchase").find(filter).toArray();
-}
-
-
-export async function getalltransfer(filter={}) {
+export async function getalltransfer(filter = {}) {
   console.log("Filter in getalltransfer:", filter);
-  return client.db("asset_management").collection("Transfer").find(filter).toArray();
+  return client
+    .db("asset_management")
+    .collection("Transfer")
+    .find(filter)
+    .toArray();
 }
 
-export async function Toputassigneddata(assigningdata){
-  return await client.db("asset_management").collection("Assigned").insertOne(assigningdata);
+export async function Toputassigneddata(assigningdata) {
+  return await client
+    .db("asset_management")
+    .collection("Assigned")
+    .insertOne(assigningdata);
 }
 
-export async function getAllAssignedData(filter){
+export async function getAllAssignedData(filter) {
   console.log("Filter in getAllAssignedData:", filter);
-  return client.db("asset_management").collection("Assigned").find(filter).toArray();
+  return client
+    .db("asset_management")
+    .collection("Assigned")
+    .find(filter)
+    .toArray();
 }
 export async function transferingdata(transferdata) {
-  return await client.db("asset_management").collection("Transfer").insertOne(transferdata);
+  return await client
+    .db("asset_management")
+    .collection("Transfer")
+    .insertOne(transferdata);
 }
-export async function toUpdate(filter,quantityUpdate){
-  return await client.db("asset_management").collection("Purchase").updateOne(filter, { $set: {Quantity:quantityUpdate} });
+export async function toUpdate(filter, quantityUpdate) {
+  return await client
+    .db("asset_management")
+    .collection("Inventory")
+    .updateOne(filter, {
+      $set: { TotalQuantity: quantityUpdate, updatedAt: new Date() },
+    });
+}
+export async function getinventory(filter = {}) {
+  return client
+    .db("asset_management")
+    .collection("Inventory")
+    .find(filter)
+    .toArray();
+}
+export async function insertpurchase(purchaseData) {
+  return await client
+    .db("asset_management")
+    .collection("Purchase")
+    .insertOne(purchaseData);
+}
+export async function Toputinventroy(assigningdata) {
+  return await client
+    .db("asset_management")
+    .collection("Inventory")
+    .insertOne(assigningdata);
+}
+export async function expendedassets(expendedData) {
+  return await client
+    .db("asset_management")
+    .collection("Expended")
+    .insertOne(expendedData);
+}
+
+export async function purchasedata(Weapon, Type, Base) {
+  return await client
+    .db("asset_management")
+    .collection("Purchase")
+    .aggregate([
+      { $match: { Weapon, Type, Base } },
+      { $group: { _id: null, total: { $sum: "$Quantity" } } },
+    ])
+    .toArray();
+}
+
+export async function transferin(Weapon, Type, Base){
+  return await client.db("asset_management").collection("Transfer").aggregate([
+        { $match: { Weapon, Type, ToBase: Base } },
+        { $group: { _id: null, total: { $sum: "$Quantity" } } }
+      ]).toArray();
+}
+export async function transferout(Weapon, Type, Base){
+  return await client.db("asset_management").collection("Transfer").aggregate([
+        { $match: { Weapon, Type, FromBase: Base } },
+        { $group: { _id: null, total: { $sum: "$Quantity" } } }
+      ]).toArray();
+}
+
+export async function assigned(Weapon, Type, Base){
+  return await client.db("asset_management").collection("Assignment").aggregate([
+        { $match: { Weapon, Type, Base } },
+        { $group: { _id: null, total: { $sum: "$Quantity" } } }
+      ]).toArray();
+}
+export async function expended(Weapon, Type, Base){
+  return await client.db("asset_management").collection("Expenditure").aggregate([
+        { $match: { Weapon, Type, Base } },
+        { $group: { _id: null, total: { $sum: "$Quantity" } } }
+      ]).toArray();
+}
+export async function getexpended(){
+  return await client
+      .db("asset_management")
+      .collection("Expended")
+      .find()
+      .toArray();
 }
--- routes.js
+++ routes.js
@@ -1,13 +1,24 @@
 import express from "express";
 import {
+  assigned,
   checkuser,
+  client,
+  expended,
+  expendedassets,
   getAllAssignedData,
   getAllPurchases,
   getalltransfer,
+  getexpended,
+  getinventory,
+  insertpurchase,
+  purchasedata,
   registeruser,
   Toputassigneddata,
+  Toputinventroy,
   toUpdate,
+  transferin,
   transferingdata,
+  transferout,
 } from "./queries.js";
 import {
   hassing,
@@ -20,8 +31,23 @@ import {
 
 const router = express.Router();
 
-router.post("/register", async (req, res) => {
+router.post("/register",authenticateToken,
+  authorizeroles("logistic officer", "admin", "commander"), async (req, res) => {
   try {
+    const user = req.user;
+    console.log("User from request:", user);
+    let token = req.headers.authorization;
+    let verification = await verifyToken(token);  
+    if (!verification) {
+      return res.status(401).send("Unauthorized");  
+    }
+    if (!user) {  
+      return res.status(401).send("Unauthorized");
+    }
+    if (user.role !== "admin") {
+      return res.status(403).send("Forbidden: Only admin can register users");
+    }
+    console.log("User role is admin, proceeding with registration");
     const { name, userid, password, role, base } = req.body;
     console.log("Received registration data:", req.body);
 
@@ -41,6 +67,7 @@ router.post("/register", async (req, res) => {
       password: hashedPassword,
       role,
       base,
+      creatredBy: user.userid,
       creatredAt: new Date(),
     };
     console.log("newUser:", newUser);
@@ -81,7 +108,7 @@ router.post("/login", async (req, res) => {
 });
 
 router.get(
-  "/purchase",
+  "/purchasehistory",
   authenticateToken,
   authorizeroles("logistic officer", "admin", "commander"),
   async (req, res) => {
@@ -146,7 +173,7 @@ router.get(
       console.log(filter);
       console.log("tranferdata history:", tranferdata);
       if (!tranferdata || tranferdata.length === 0) {
-        return res.status(404).send("No purchase history found");
+        return res.status(404).send("No Transfer history found");
       }
       res.status(200).send(tranferdata);
     } catch (err) {
@@ -249,27 +276,27 @@ router.post(
       if (!user) {
         return res.status(401).send("Unauthorized");
       }
-      
+
       if (user.role === "commander" || user.role === "logistic officer") {
-        filter.Base = new RegExp(`^${FromBase}$`, "i"); //here used user.base instead of userdata.base
+        filter.Base = new RegExp(`^${FromBase}$`, "i");
       } else if (user.role === "admin") {
         filter.Base = new RegExp(`^${FromBase}$`, "i");
       }
-      
+
       filter.Weapon = new RegExp(`^${Weapon}$`, "i");
-      const purchase = await getAllPurchases(filter);
-      console.log("purchase:", purchase);
-      if (purchase.length === 0) {
+      const inventory = await getinventory(filter);
+      console.log("inventory:", inventory);
+      if (inventory.length === 0) {
         return res
           .status(404)
           .send(
             "No purchase history found for this weapon at the specified base"
           );
       }
-     
-  const transferData = {
+
+      const transferData = {
         Weapon,
-        purchaseId: purchase[0]._id, 
+        inventoryId: inventory[0]._id,
         Type,
         Quantity,
         FromBase,
@@ -285,9 +312,10 @@ router.post(
           .status(403)
           .send("Forbidden: You can only transfer assets from your own base");
       }
-      
-      if (purchase[0].Quantity >= transferData.Quantity) {
-        const quantityUpdate = purchase[0].Quantity - transferData.Quantity;
+
+      if (inventory[0].TotalQuantity >= transferData.Quantity) {
+        const quantityUpdate =
+          inventory[0].TotalQuantity - transferData.Quantity;
         await transferingdata(transferData);
         const updated = await toUpdate(filter, quantityUpdate);
         console.log("updated:", updated);
@@ -303,11 +331,231 @@ router.post(
     }
   }
 );
-/*router.get("/dashboard",authenticateToken,authorizeroles("logistic officer","commander","admin"),async(req,res)=>{
-  try{
-  }catch(err){
-    console.error("Error during dashboard:", err);
-    res.status(500).send("Internal server error");
+router.post(
+  "/purchase",
+  authenticateToken,
+  authorizeroles("logistic officer", "admin"),
+  async (req, res) => {
+    try {
+      const { Weapon, Type, Quantity, Base } = req.body;
+      console.log("Received purchase data:", req.body);
+      const user = req.user;
+      let token = req.headers.authorization;
+      let verification = await verifyToken(token);
+      if (!verification) {
+        return res.status(401).send("Unauthorized");
+      }
+      if (!user) {
+        return res.status(401).send("Unauthorized");
+      }
+      if (user.role !== "admin" && user.role !== "logistic officer") {
+        return res
+          .status(403)
+          .send("Forbidden: Only admin/logistic officer can make purchases");
+      }
+      const purchaseData = {
+        Weapon,
+        Type,
+        Quantity,
+        PurchaseDate: new Date(),
+        Base,
+      };
+      console.log("Purchase data:", purchaseData);
+      const purchaseHistory = await insertpurchase(purchaseData);
+      const checkinvetory = await getinventory({
+        Weapon: new RegExp(`^${Weapon}$`, "i"),
+        Base: new RegExp(`^${Base}$`, "i"),
+      });
+      if (checkinvetory.length === 0) {
+        const newInventory = {
+          Base,
+          Weapon,
+          Type,
+          TotalQuantity: Quantity,
+        };
+        await Toputinventroy(newInventory);
+        console.log("New inventory item created:", newInventory);
+      }
+      if (checkinvetory[0]) {
+        const quantityUpdate =
+          checkinvetory[0].TotalQuantity + purchaseData.Quantity;
+        console.log("inventory", checkinvetory.TotalQuantity);
+        console.log("Total quantity to be updated:", quantityUpdate);
+        const filter = {
+          Weapon: new RegExp(`^${Weapon}$`, "i"),
+          Base: new RegExp(`^${Base}$`, "i"),
+        };
+        console.log("Filter for inventory update:", filter);
+        await toUpdate(filter, quantityUpdate);
+      }
+      console.log("Purchase history:", purchaseHistory);
+      res.status(200).send({ message: "Asset purchased successfully" });
+    } catch (err) {
+      console.error("Error during purchase:", err);
+      res.status(500).send("Internal server error");
+    }
   }
-})*/
+);
+router.post(
+  "/expendedasset",
+  authenticateToken,
+  authorizeroles( "commander", "admin"),
+  async (req, res) => {
+    try {
+      const { Weapon, Type, Quantity,Reason, Base } = req.body;
+      console.log("Received expended asset data:", req.body);
+      const user = req.user;
+      let token = req.headers.authorization;
+      let verification = await verifyToken(token);
+      if (!verification) {
+        return res.status(401).send("Unauthorized");
+      }
+      if (!user) {
+        return res.status(401).send("Unauthorized");
+      }
+      if (user.role !== "admin" && user.role !== "commander") {
+        return res
+          .status(403)
+          .send("Forbidden: Only admin/commander can expend assets");
+      }
+
+      const expendedData = {
+        Weapon,
+        Type,
+        Quantity,
+        Reason,
+        Base,
+        ExpendedDate: new Date(),
+        ExpendedBy: user.userid,
+      };
+
+      console.log("Expended asset data:", expendedData);
+      await expendedassets(expendedData);
+
+      const checkinvetory = await getinventory({
+        Weapon: new RegExp(`^${Weapon}$`, "i"),
+        Base: new RegExp(`^${Base}$`, "i"),
+      });
+      if (checkinvetory.length === 0) {
+        return res
+          .status(404)
+          .send("No inventory found for this weapon at the specified base");
+      }
+      console.log(" inventory item :", checkinvetory[0]);
+      if (checkinvetory[0]) {
+        const quantityUpdate =
+          checkinvetory[0].TotalQuantity - expendedData.Quantity;
+        console.log("inventory", checkinvetory.TotalQuantity);
+        console.log("Total quantity to be updated:", quantityUpdate);
+        const filter = {
+          Weapon: new RegExp(`^${Weapon}$`, "i"),
+          Base: new RegExp(`^${Base}$`, "i"),
+        };
+        console.log("Filter for inventory update:", filter);
+        await toUpdate(filter, quantityUpdate);
+      }
+      res.status(200).send({ message: "Asset expended successfully" });
+    } catch (err) {
+      console.error("Error during expended asset:", err);
+      res.status(500).send("Internal server error");
+    }
+  }
+);
+
+router.get('/dashboard', authenticateToken,authorizeroles("logistic officer","commander","admin"),async (req, res) => {
+  try {
+    const user = req.user; 
+    console.log("User from request:", user);
+    let token = req.headers.authorization; 
+    let verification = await verifyToken(token);
+      if (!verification) {
+        return res.status(401).send("Unauthorized");
+      }
+      if (!user) {
+        return res.status(401).send("Unauthorized");
+      } 
+    let filter = {};
+    if (user.role !== "admin") {
+      filter.Base = user.base;
+    }
+    const inventory = await client.db("asset_management").collection("Inventory").find(filter).toArray();
+console.log("Inventory data:", inventory);
+    const report = [];
+
+    for (const item of inventory) {
+      const { Weapon, Type, Base } = item;
+
+      const purchase = await purchasedata(Weapon, Type, Base);
+      const transferedIn = await transferin(Weapon, Type, Base);
+      const transferedOut = await transferout(Weapon, Type, Base);
+      const assigneddata = await assigned(Weapon, Type, Base);
+      const expendeddata = await expended(Weapon, Type, Base);
+
+      const PurchaseQty = purchase[0]?.total || 0;
+      const TransferedIn = transferedIn[0]?.total || 0;
+      const TransferedOut = transferedOut[0]?.total || 0;
+      const AssignedQuantity = assigneddata[0]?.total || 0;
+      const ExpendedQuantity = expendeddata[0]?.total || 0;
+      const ClosingBalance = item.TotalQuantity;
+      const NetMovement = PurchaseQty + TransferedIn - TransferedOut - AssignedQuantity - ExpendedQuantity;
+      const OpeningBalance = ClosingBalance - NetMovement;
+
+      report.push({
+        Weapon,
+        Type,
+        Base,
+        OpeningBalance,
+        PurchaseQty,
+        TransferedIn,
+        TransferedOut,
+        AssignedQuantity,
+        ExpendedQuantity,
+        NetMovement,
+        ClosingBalance
+      });
+    }
+console.log("Generated report:", report);
+    if (report.length === 0) {
+      return res.status(404).send("No asset movement data found ");
+    }
+    res.status(200).json(report);
+  } catch (error) {
+    console.error("Error generating asset movement summary:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+router.get("/expendedassets", authenticateToken,
+  authorizeroles("commander", "admin"),async (req, res) => {
+  try {
+    const user = req.user;
+    const filter={};
+    console.log("User from request:", user);
+    let token = req.headers.authorization;
+    let verification = await verifyToken(token);
+    if (!verification) {
+      return res.status(401).send("Unauthorized");
+    }
+    if (!user) {
+      return res.status(401).send("Unauthorized");
+    }
+    if (user.role !== "admin" && user.role !== "commander") {
+      return res
+        .status(403)
+        .send("Forbidden: Only admin/commander can view expended assets");
+    }
+    if(user.role === "commander" ){
+      filter.Base = new RegExp(`^${user.base}$`, "i");
+    }
+    const expendedAssets = await getexpended();
+  
+    console.log("Expended assets:", expendedAssets);
+    if (!expendedAssets[0] || expendedAssets.length === 0) {
+      return res.status(404).send("No expended assets found");
+    }
+    res.status(200).send(expendedAssets);
+  }catch (error) {
+    console.error("Error fetching expended assets:", error);
+    res.status(500).send("Internal Server Error");
+  }});
+
 export default router;
