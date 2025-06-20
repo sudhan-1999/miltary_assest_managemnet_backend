0b77a14825782903b88fd25c4b86e1909b22c0e9
sudhan-1999 <jsudhan68@gmail.com>
2025-06-20 08:48:35 +0530

    few import aremade

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import router from './routes.js';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors())
 
 app.use("/api", router);
 app.use("/api", router);
